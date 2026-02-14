87c0de9cecd627e8f12f3d961a5814765c4172ad
gaursushant97-del <gaursushant97@gmail.com>
2026-02-14 22:56:21 +0530

    Assignment Task two_Personalized Greeting

---
+++ Assignment2_task1.py
@@ -0,0 +1,6 @@
+# hi there this is my Assignment 2 (task1)
+
+num=int(input("Enter a number to check whether it's a even or odd:"))
+print(num,"is an Even number") if num%2==0 else print(num,"is an odd number")
+
+
