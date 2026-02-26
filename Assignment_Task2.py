71e9f60e37c6057bf1d474cff130135ac400b846
gaursushant97-del <gaursushant97@gmail.com>
2026-02-26 12:53:26 +0530

    Assignment Task two_Personalized Greeting

--- Assignment_Task2.py
+++
@@ -1,6 +0,0 @@
-#Hi there, this is my assignment task 2
-
-First_name = input("Please enter your first name: ")
-Last_name = input("Please enter your last name: ")
-
-print("Hello,",First_name + " " + Last_name+ "!", "Welcome to python program." )
---
+++ Assignment_Task2_updated.py
@@ -0,0 +1,4 @@
+First_name = input("Please enter your first name: ")
+Last_name = input("Please enter your last name: ")
+
+print("Hello,", First_name + " " + Last_name + "!", "Welcome to python program.")
