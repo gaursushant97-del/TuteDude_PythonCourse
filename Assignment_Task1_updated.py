b8eca9965751cb1c055c593d32c4ed534ff58005
gaursushant97-del <gaursushant97@gmail.com>
2026-02-26 12:51:06 +0530

    Assignment Task one_Mathematical operations

---
+++ Assignment_Task1_updated.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+print("this is the sum of the given two numbers:", num1 + num2)
+
+print("this is the difference of the given two numbers:", num1 - num2)
+
+print("this is the product of the given two numbers:", num1 * num2)
+
+print("this is the quotient of the given two numbers:", num1 / num2)
